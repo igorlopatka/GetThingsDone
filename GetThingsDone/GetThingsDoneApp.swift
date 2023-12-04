8a78b6fee5e1d90ce6536955ac8028af9bb13a4e
Igor Łopatka <63002337+igorlopatka@users.noreply.github.com>
2023-12-04 18:22:46 +0100

    Initial Commit

---
+++ GetThingsDone/GetThingsDoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetThingsDoneApp.swift
+//  GetThingsDone
+//
+//  Created by Igor Łopatka on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GetThingsDoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
