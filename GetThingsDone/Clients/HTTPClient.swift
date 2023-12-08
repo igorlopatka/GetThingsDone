62866186581f8cf163a3c045e92fbee438756ace
Igor Łopatka <63002337+igorlopatka@users.noreply.github.com>
2023-12-08 16:38:19 +0100

    add generic networking layer

---
+++ GetThingsDone/Clients/HTTPClient.swift
@@ -0,0 +1,78 @@
+//
+//  HTTPClient.swift
+//  GetThingsDone
+//
+//  Created by Igor Łopatka on 08/12/2023.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badRequest
+    case serverError(String)
+    case decodingError
+    case invalidResponse
+}
+
+enum HTTPMethod {
+    case get([URLQueryItem])
+    case post(Data?)
+    case delete
+    
+    var name: String {
+        switch self {
+        case .get:
+            "GET"
+        case .post:
+            "POST"
+        case .delete:
+            "DELETE"
+        }
+    }
+}
+
+struct Resource <T: Codable> {
+    let url: URL
+    let method: HTTPMethod = .get([])
+    let modelType: T.Type
+}
+
+struct HTTPClient {
+    
+    func load<T: Codable>(_ resource : Resource<T>) async throws -> T {
+        
+        var request = URLRequest(url: resource.url)
+        
+        switch resource.method {
+            
+        case .get(let queryItems):
+            var components = URLComponents(url: resource.url, resolvingAgainstBaseURL: false)
+            components?.queryItems = queryItems
+            guard let url = components?.url else {
+                throw NetworkError.badRequest
+            }
+            request = URLRequest(url: url)
+        case .post(let data):
+            request.httpMethod = resource.method.name
+            request.httpBody = data
+        case .delete:
+            request.httpMethod = resource.method.name
+        }
+        
+        let configuration = URLSessionConfiguration.default
+        // configuration.httpAdditionalHeaders =
+        let session = URLSession(configuration: configuration)
+        
+        let (data, response) = try await session.data(for: request)
+        
+        guard let _ = response as? HTTPURLResponse else {
+            throw NetworkError.invalidResponse
+        }
+        
+        guard let result = try? JSONDecoder().decode(resource.modelType, from: data) else {
+            throw NetworkError.decodingError
+        }
+        
+        return result
+    }
+}
